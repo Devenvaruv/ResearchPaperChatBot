7be805c66e8b073f55110ef548ee52961435d559
your_github_username <devenvaruv@gmail.com>
2024-10-08 15:44:55 -0700

    added button to upload file

--- frontend/ResearchPaperFrontend/src/App.js
+++ frontend/ResearchPaperFrontend/src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
 
 function App() {
+
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setPdfFile(event.target.files[0]);
+  };
+
   return (
     <div className="App">
       <h2>"PLACEHOLDER PLACEHOLDER"</h2>
       <h3>"PLACEHOLDER LOREM"</h3>
+      <div>
+        <input type="file" accept=".pdf" onChange={handleFileChange}/>
+        <button style={{ margineLeft: '10px'}}> Upload PDF</button>
+      </div>
     </div>
   );
 }
