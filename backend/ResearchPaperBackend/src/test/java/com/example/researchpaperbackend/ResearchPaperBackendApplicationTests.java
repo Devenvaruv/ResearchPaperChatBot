2c202336780eea60a7589fa4e1557c380312fde3
your_github_username <devenvaruv@gmail.com>
2024-10-08 13:44:48 -0700

    added Springboot backend

---
+++ backend/ResearchPaperBackend/src/test/java/com/example/researchpaperbackend/ResearchPaperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.researchpaperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchPaperBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
