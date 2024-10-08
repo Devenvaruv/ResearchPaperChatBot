77d7b097be5cd013ae57140bd1bd80ad35c606a0
your_github_username <devenvaruv@gmail.com>
2024-10-08 15:00:33 -0700

    added Frontend

--- backend/ResearchPaperBackend/src/main/java/com/example/researchpaperbackend/ResearchPaperController.java
+++ backend/ResearchPaperBackend/src/main/java/com/example/researchpaperbackend/ResearchPaperController.java
@@ -1,8 +1,13 @@
 package com.example.researchpaperbackend;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -19,8 +24,20 @@ public class ResearchPaperController {
             return ResponseEntity.badRequest().body("Empty file");
         }
 
-        System.out.println(file.getOriginalFilename());
-        return ResponseEntity.ok("File uploaded successfully: " + file.getOriginalFilename());
+        try {
+            String extractedText = extractTextFromPdf(file);
+            
+            return ResponseEntity.ok("Extracted Text:\n" + extractedText);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to process the PDF file");
+        }
+    }
+
+    private String extractTextFromPdf(MultipartFile file) throws IOException {
+        try(PDDocument document = PDDocument.load(file.getInputStream()) ) {
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            return pdfStripper.getText(document);
+        }
     }
 }
 
