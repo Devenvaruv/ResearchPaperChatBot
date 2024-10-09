234f40bf2f1d0380d09e4ea5fb4e90ce07400cc4
your_github_username <devenvaruv@gmail.com>
2024-10-09 10:52:09 -0700

    added placeholder buttons to send message to backend

--- frontend/ResearchPaperFrontend/src/App.js
+++ frontend/ResearchPaperFrontend/src/App.js
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
   const [pdfFile, setPdfFile] = useState(null);
+  const [chatInput, setChatInput] = useState('');
+  const [chatLog, setChatLog] = useState([]);
 
   const handleFileChange = (event) => {
     setPdfFile(event.target.files[0]);
   };
+
+  const handleInputChange = (event) => {
+    setChatInput(event.target.value);
+  }
+
+  const handleUpload = async () => {
+    if(!pdfFile) {
+      alert("Please select a file");
+      return;
+    }
+  
+
+    const formData = new FormData();
+    formData.append('file', pdfFile);
+    formData.append('index', 'test');
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/upload-pdf', formData);
+      alert(response.data);
+    } catch (error) {
+      console.log("error uploading: " , error);
+      alert("failed to upload pdf.");
+    }
+  }
 
   return (
     <div className="App">
@@ -16,6 +43,24 @@ function App() {
         <input type="file" accept=".pdf" onChange={handleFileChange}/>
         <button style={{ margineLeft: '10px'}}> Upload PDF</button>
       </div>
+
+      <div>
+        {chatLog.map((entry, index) => (
+          <div key={index} style={{ margin: '10px 0'}}>
+            <strong>{entry.sender === 'user' ? 'You' : 'Bot'}:</strong> {entry.message}
+          </div>  
+        ))}
+      </div>
+
+      <form >
+        <input type="text" value={chatInput} onChange={handleInputChange}
+        placeholder="Type your message..."
+        style={{ flex: '1', padding: '10px', marginRight: '10px'}}
+        />
+        <button type="submit" style={{ padding: '10px 20px'}}>
+          Send
+        </button>
+      </form>
     </div>
   );
 }
